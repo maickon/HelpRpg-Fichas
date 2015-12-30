4184b869f593ddb9aee3c003bbc09e92b41dc73b
maickon <maickonmaickon@hotmail.com>
2015-12-30 14:30:42 -0200

    editando algumas definiçoes no arquivo de configuraçao

--- config.php
+++ config.php
@@ -6,17 +6,17 @@ define('JSPATH', "js/");
 define('CSSPATH', "css/");
 define('IMGPATH', "img/");
 define('HOMEPATH', "pages/home");
-define('ROOTPATH',"C:/xampp/htdocs/HelpRpg/"); 			
-define('ROOTPATHURL',"http://127.0.0.1/HelpRpg/");		
+define('ROOTPATH',"C:/xampp/htdocs/HelpRpg-Fichas/"); 			
+define('ROOTPATHURL',"http://127.0.0.1/HelpRpg-Fichas/");		
 define('SEPARETOR',"/");
 define('ABOUTPATH', ROOTPATHURL."pages/sobre/");
 define('HOWTOUSE', ROOTPATHURL.'pages/como-usar/');
 define('DOWNLOADPATH','arquivos/');
-define('NPCGENERATEPATH','http://helprpg.com.br/old/index.php?p=fichas');
-define('MONSTERGENERATEPATH','http://helprpg.com.br/old/index.php?p=monstros');
-define('ROLLDICE','http://helprpg.com.br/roll-dice');
-define('DUNGEONPATH','http://helprpg.com.br/pages/masmorras/');
-define('MAPWORDPATH','http://helprpg.com.br/pages/mapa_mundi_fantasia/');
+define('NPCGENERATEPATH',ROOTPATHURL.'old/index.php?p=fichas');
+define('MONSTERGENERATEPATH',ROOTPATHURL.'old/index.php?p=monstros');
+define('ROLLDICE',ROOTPATHURL.'roll-dice');
+define('DUNGEONPATH',ROOTPATHURL.'pages/masmorras/');
+define('MAPWORDPATH',ROOTPATHURL.'pages/mapa_mundi_fantasia/');
 define('YOUTUBE','https://www.youtube.com/channel/UCVx62ydCm9D9JubvsEQXwlA');
 define('FACEBOOK','https://www.facebook.com/Help-RPG-539011612872186/?ref=hl');
 define('HELPRPGBLOG','https://helprpg.wordpress.com/');
@@ -79,12 +79,12 @@ define('LOGIN_VALIDATION_PATH', ROOTPATHURL.'pages/login/login.php');
 define('LOGOFF_PATH', 'pages/login/logoff.php');
 define('HOME_PATH', 'pages/home/');
 define('PAINEL_PATH', ROOTPATHURL.'pages/painel/');
-define('BASE_PATH', 'http://127.0.0.1/HelpRpg/'); //http://helprpg.com.br
+define('BASE_PATH', 'http://127.0.0.1/HelpRpg-Fichas/'); //http://helprpg.com.br
 
 //Usu�rio e senha do banco de dados
 define('DB_USER', "root");		
 define('DB_PASS', "");	  		
-define('DB_NAME', "helprpg"); 	
+define('DB_NAME', "help_rpg"); 	
 define('DB_HOST', "localhost");
 
 
