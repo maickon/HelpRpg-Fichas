b8b099c7cddf203ba9badba0d396925be0c6c674
maickon <maickonmaickon@hotmail.com>
2015-12-31 02:20:40 -0200

    Arquivo de traducao na linguagem pt-br

--- termos/pt-br.php
+++ termos/pt-br.php
@@ -1,9 +1,29 @@
 <?php
+
+//Nome/Titulo do site/projeto
+define('PROJECTTITLE', "Help RPG Fichas");
+define('PROGRAMER', 'Desenvolvido por <a href="http://www.mrcurriculo.orgfree.com/" target="blank">Maickon Rangel</a> todos os direitos reservados');
+define('COPY', "&copy 2013-2015");
+
 //Labels de menu no painel adm
 define('NOVO', "Novo");
 define('EDITAR', "Editar");
 define('DELETAR', "Apagar");
 define('VER', "Visualizar");
+define("BACK", "Voltar");
+
+define('VOCE', "Você");
+define('SAIR', "Sair");
+define('LOGAR', 'Logar');
+define('USUARIOS', "Usuários");
+define('ESTATISTICAS', "Estatísticas");
+define('EDITAR_DADOS', "Editar dados");
+define('CANCELAR_CONTA', "Cancelar conta");
+define('PERSONAGEM_MENU', "Personagem");
+define('CRIAR_PERSONAGEM', "Criar um personagem");
+define('CRIAR_MONSTRO', "Criar um monstro");
+define('CRIAR_BOSS', "Criar um BOSS");
+
 define('UTILITARIOS','Utilitários');
 define('ROLAR_DADOS','Rolar Dados');
 define('GERADOR_DE_MUNDOS','Gerador de Mundos');
@@ -13,20 +33,37 @@ define('GERADOR_DE_MONSTROS','Gerador de Monstros');
 define('DOWNLOADS','Downloads');
 define('DOWNLOAD','Baixar');
 define('CAMPANHAS','Campanhas');
-//botoes
-define("BACK", "Voltar");
-
 
 define('CADASTRAR', "Cadastrar");
 define('AVENTURAS', "Aventuras");
 
 //midias sociais
 define('MEDIAS_SOCIAIS', "Mídias Sociais");
-define('FACEBOOK', "");
+define('FACEBOOK', "Página no Facebook");
+define('YOUTUBE', "Canal no Youtube");
+define('WORDPRESS_BLOG', "Blog Help Rpg");
+
+//sites parceiros
+define('TAVERNA_DO_ELFO', "Taverna do Elfo");
+define('BIBLIOTECA_ELFICA', "Biblioteca Elfica");
+
+//app
+define('MASMORRAS',"Mapas Masmorras");
+define('MAPAS', "Mapas Mundi");
+define('FICHAS', "Fichas");
+define('ITENS', "Itens");
+define('MAGIAS_MENU', "Magias");
+
+//paginas
+define('HOME','Home');
+define('CADASTRO','Cadastro');
+define('SOBRE','Sobre');
+define('COMO_USAR','Como Usar');
 
 //termos pagina aventuras
 define("TITULO", "Título");
 define("MESTRE", "Mestre");
+define('LEVEL_INDICADO', "Nível indicado");
 define("TIPO", "Tipo");
 define("CRIADO_EM", "Criado em");
 define("SISTEMA", "Sistema");
