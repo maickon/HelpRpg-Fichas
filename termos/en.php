2d00d6311e2e84dfc2327151871d71f6e9b8d10c
maickon <maickonmaickon@hotmail.com>
2015-12-31 02:20:56 -0200

    Arquivo de traducao na linguagem en

--- termos/en.php
+++ termos/en.php
@@ -1,22 +1,68 @@
 <?php
+define('PROJECTTITLE', "Help RPG Sheets");
+define('PROGRAMER', 'Development by <a href="http://www.mrcurriculo.orgfree.com/" target="blank">Maickon Rangel</a> All rights reserved.');
+define('COPY', "&copy 2013-2015");
+
 //Menu labels from menage pane 
 define('NOVO', "New");
 define('EDITAR', "Edit");
 define('DELETAR', "Delete");
 define('VER', "View");
 define('DOWNLOAD','Download');
-
-//buttons
 define("BACK", "Back");
 
+define('VOCE', "You");
+define('SAIR', "Logout");
+define('LOGAR', 'Login');
+define('USUARIOS', "Users");
+define('ESTATISTICAS', "Statistics");
+define('EDITAR_DADOS', "Edit data");
+define('CANCELAR_CONTA', "Cancel account");
+define('PERSONAGEM_MENU', "Character");
+define('CRIAR_PERSONAGEM', "New Character");
+define('CRIAR_MONSTRO', "New Monster");
+define('CRIAR_BOSS', "New BOSS");
 
-define('CADASTRAR', "Sing in");
+define('UTILITARIOS','Utilities');
+define('ROLAR_DADOS','Roll Dice');
+define('GERADOR_DE_MUNDOS','New world');
+define('GERADOR_DE_MASMORRAS','Dungeon Generator');
+define('GERADOR_DE_NPCS','Npc Generator');
+define('GERADOR_DE_MONSTROS','Monster Generator');
+define('DOWNLOADS','Downloads');
+define('CAMPANHAS','Campaigns');
+
+define('CADASTRAR', "Register");
 define('AVENTURAS', "Adventures");
+
+//social medias
+define('MEDIAS_SOCIAIS', "Social media");
+define('FACEBOOK', "Facebook");
+define('YOUTUBE', "Youtube");
+define('WORDPRESS_BLOG', "Blog Help Rpg");
+
+//sites parceiros
+define('TAVERNA_DO_ELFO', "Elf Tavern");
+define('BIBLIOTECA_ELFICA', "Elf Library");
+
+//app
+define('MASMORRAS',"Dungeons maps");
+define('MAPAS', "World Maps");
+define('FICHAS', "Fichas");
+define('ITENS', "Sheets");
+define('MAGIAS_MENU', "spells");
 
 //terms to page advetures
 define("TITULO", "Ttitle");
 define("MESTRE", "Master");
+define('LEVEL_INDICADO', "Indicate Lv");
 define("TIPO", "Type");
 define("CRIADO_EM", "Create at");
 define("SISTEMA", "System");
-define("CRIADOR", "Create by");
+define("CRIADOR_MENU", "Create by");
+
+//pages
+define('HOME','Home');
+define('CADASTRO','Register');
+define('SOBRE','About');
+define('COMO_USAR','How To Use');
