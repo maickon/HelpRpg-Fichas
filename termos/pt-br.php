99ad97076bff91fdc40a683f4770824ab53090b5
maickon <maickonmaickon@hotmail.com>
2016-01-04 20:29:24 -0200

    arquivo de traducao do site para o idioma portugues BR

--- termos/pt-br.php
+++ termos/pt-br.php
@@ -3,14 +3,16 @@
 //Nome/Titulo do site/projeto
 define('PROJECTTITLE', "Help RPG Fichas");
 define('PROGRAMER', 'Desenvolvido por <a href="http://www.mrcurriculo.orgfree.com/" target="blank">Maickon Rangel</a> todos os direitos reservados');
-define('COPY', "&copy 2013-2015");
+define('COPY', "&copy 2013-".date('Y'));
 
-//Labels de menu no painel adm
+//Labels de menu no painel adm e msg de alerta
 define('NOVO', "Novo");
 define('EDITAR', "Editar");
 define('DELETAR', "Apagar");
 define('VER', "Visualizar");
 define("BACK", "Voltar");
+define('SUCESSO_MSG', "Registro deletado com sucesso.");
+define('PERIGO_MSG', "Um erro ocoreu, contate o administrador.");
 
 define('VOCE', "Você");
 define('SAIR', "Sair");
@@ -23,6 +25,7 @@ define('PERSONAGEM_MENU', "Personagem");
 define('CRIAR_PERSONAGEM', "Criar um personagem");
 define('CRIAR_MONSTRO', "Criar um monstro");
 define('CRIAR_BOSS', "Criar um BOSS");
+define('SISTEMA_DE_JOGO', "Sistema de jogo");
 
 define('UTILITARIOS','Utilitários');
 define('ROLAR_DADOS','Rolar Dados');
@@ -36,6 +39,12 @@ define('CAMPANHAS','Campanhas');
 
 define('CADASTRAR', "Cadastrar");
 define('AVENTURAS', "Aventuras");
+define('CENARIOS', "Cenários");
+define('CENARIO', "Cenário");
+define('CONTOS', "Contos");
+define('CONTO', "Conto");
+define('HISTORIAS_PAGINA', "Histórias");
+define('EDITADO_POR', "Editado por");
 
 //midias sociais
 define('MEDIAS_SOCIAIS', "Mídias Sociais");
@@ -67,3 +76,16 @@ define('LEVEL_INDICADO', "Nível indicado");
 define("TIPO", "Tipo");
 define("CRIADO_EM", "Criado em");
 define("SISTEMA", "Sistema");
+define("CRIADOR_MENU", "Criado por");
+define("NIVEL_INDICADO_AVENTURA", "Nível indicado para a aventura");
+define('MESTRE_DA_AVENTURA', "Mestre da aventura");
+define('TIPO_DE_AVENTURA', "Tipo de aventura");
+//define('SISTEMA_DE_RPG', "Sistema de RPG");
+define('HISTORIA_DA_AVENTURA', "História da aventura");
+define('CRIADO_NO_DIA', "Criado no dia");
+define('CADASTRADO_POR', "Cadastrado por");
+define('AVENTURA_INDICADA_NIVEL', "Aventura indicada para o nível");
+//termos pagina contos
+define('AUTOR', "Autor");
+define('DONO', "Dono");
+define('DESCRICAO_BREVE', "Descrição breve");
