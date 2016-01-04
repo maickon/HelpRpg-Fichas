c4d0ec776d57b96322265548822ff3acc5dc91e5
maickon <maickonmaickon@hotmail.com>
2016-01-04 20:29:05 -0200

    arquivo de traducao do site para o idioma ingles

--- termos/en.php
+++ termos/en.php
@@ -3,13 +3,15 @@ define('PROJECTTITLE', "Help RPG Sheets");
 define('PROGRAMER', 'Development by <a href="http://www.mrcurriculo.orgfree.com/" target="blank">Maickon Rangel</a> All rights reserved.');
 define('COPY', "&copy 2013-2015");
 
-//Menu labels from menage pane 
+//Menu labels from menage pane and msg alert
 define('NOVO', "New");
 define('EDITAR', "Edit");
 define('DELETAR', "Delete");
 define('VER', "View");
 define('DOWNLOAD','Download');
 define("BACK", "Back");
+define('SUCESSO_MSG', "Successfully deleted record.");
+define('PERIGO_MSG', "An error occurred, contact your administrator.");
 
 define('VOCE', "You");
 define('SAIR', "Logout");
@@ -22,6 +24,7 @@ define('PERSONAGEM_MENU', "Character");
 define('CRIAR_PERSONAGEM', "New Character");
 define('CRIAR_MONSTRO', "New Monster");
 define('CRIAR_BOSS', "New BOSS");
+define('SISTEMA_DE_JOGO', "Game system");
 
 define('UTILITARIOS','Utilities');
 define('ROLAR_DADOS','Roll Dice');
@@ -34,6 +37,11 @@ define('CAMPANHAS','Campaigns');
 
 define('CADASTRAR', "Register");
 define('AVENTURAS', "Adventures");
+define('CENARIOS', "Scenario");
+define('CENARIO', "Scenario");
+define('CONTOS', "Tales");
+define('HISTORIAS_PAGINA', "Stories");
+define('EDITADO_POR', "Edited by");
 
 //social medias
 define('MEDIAS_SOCIAIS', "Social media");
@@ -60,9 +68,21 @@ define("TIPO", "Type");
 define("CRIADO_EM", "Create at");
 define("SISTEMA", "System");
 define("CRIADOR_MENU", "Create by");
+define("NIVEL_INDICADO_AVENTURA", "Level indicated for adventure");
+define('MESTRE_DA_AVENTURA', "Adventure Master");
+define('TIPO_DE_AVENTURA', "Kind of adventure");
+define('SISTEMA_DE_RPG', "RPG system");
+define('HISTORIA_DA_AVENTURA', "Adventure story");
+define('CRIADO_NO_DIA', "Created on");
+define('CADASTRADO_POR', "Joined by");
 
 //pages
 define('HOME','Home');
 define('CADASTRO','Register');
 define('SOBRE','About');
 define('COMO_USAR','How To Use');
+
+//terms to page tales
+define('AUTOR', "Author");
+define('DONO', "Owner");
+define('DESCRICAO_BREVE', "brief description");
