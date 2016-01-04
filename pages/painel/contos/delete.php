32fee9a85803afdb6743abfb172e227e3d178bb0
maickon <maickonmaickon@hotmail.com>
2016-01-04 20:22:54 -0200

    novo arquivo para deletar contos cadastrados no sistema

---
+++ pages/painel/contos/delete.php
@@ -0,0 +1,21 @@
+<?php
+require_once '../../../header.php';
+require_once '../helper.php';
+
+global $s;
+
+$s->restricted_access();
+
+$delete_contos = new Contos();
+$objeto = $delete_contos->select($delete_contos->getTable(), null, [['id','=', $_GET['id'] ? $_GET['id'] : ' ']]);
+
+$pass = helper_check_permitions($objeto[0]['dono']);
+if(!$pass)
+	header("Location: ".ROOTPATHURL.CONTOSPATH);
+
+$delete = $delete_contos->delete_data($objeto);
+if($delete == 1):
+	header("Location: ".ROOTPATHURL.CONTOSPATH.'?status=deleted');
+else:
+	header("Location: ".ROOTPATHURL.CONTOSPATH.'?status=error');
+endif;
