e9526a62f774190269dbf105745129cee969cb14
maickon <maickonmaickon@hotmail.com>
2016-01-04 20:25:09 -0200

    novo arquivo para deletar historias cadastradas no sistema

---
+++ pages/painel/historias/delete.php
@@ -0,0 +1,21 @@
+<?php
+require_once '../../../header.php';
+require_once '../helper.php';
+
+global $s;
+
+$s->restricted_access();
+
+$delete_historia = new Historias();
+$objeto = $delete_historia->select($delete_historia->getTable(), null, [['id','=', $_GET['id'] ? $_GET['id'] : ' ']]);
+
+$pass = helper_check_permitions($objeto[0]['dono']);
+if(!$pass)
+	header("Location: ".ROOTPATHURL.HISTORIASPATH);
+
+$delete = $delete_historia->delete_data($objeto);
+if($delete == 1):
+	header("Location: ".ROOTPATHURL.HISTORIASPATH.'?status=deleted');
+else:
+	header("Location: ".ROOTPATHURL.HISTORIASPATH.'?status=error');
+endif;
