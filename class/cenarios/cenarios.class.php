cd5b65426d5318c8dd847d79065b314c65f0c56c
maickon <maickonmaickon@hotmail.com>
2016-01-04 20:15:11 -0200

    novo arquivo de classe para maniputar cenarios de RPG

---
+++ class/cenarios/cenarios.class.php
@@ -0,0 +1,21 @@
+<?php
+class Cenarios extends Db_persistence{
+	private $attr;
+	
+	function __construct($path = "", $warning_msg = 'Já existe um cenário com este nome!'){
+		parent::__construct(strtolower(get_class($this)), $path, $warning_msg);
+	}
+	
+	function __set($attr_name, $attr_value){
+	
+		$this->attr["{$attr_name}"] = $attr_value;
+	}
+	
+	function __get($attr){
+		return $this->attr["{$attr}"];
+	}
+	
+	function getTable(){
+		return $this->table;
+	}
+}
