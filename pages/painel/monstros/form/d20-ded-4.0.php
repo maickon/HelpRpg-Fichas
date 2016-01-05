fd62fb8382b1d7e8685f51979961f5592ce8a7b0
maickon <maickonmaickon@hotmail.com>
2016-01-05 00:48:34 -0200

    editando arquivo de formulario para o sistema D&D 4.0

--- pages/painel/monstros/form/d20-ded-4.0.php
+++ pages/painel/monstros/form/d20-ded-4.0.php
@@ -6,84 +6,37 @@ $form->input(['name' => 'sistema', 'type' => 'hidden', 'value'=> 'Dungeons and D
 $form->input(['name' => 'tipo', 'type' => 'hidden', 'value'=> 'Personagem jogador']);
 
 $atributos = [
-				['Força','forca'],['Destreza','destreza'],['Constituição','constituicao'],
-				['Inteligência','inteligencia'],['Sabedoria','sabedoria'],['Carisma','carisma']
+				['Força','forca',2],['Destreza','destreza',2],['Constituição','constituicao',2],
+				['Inteligência','inteligencia',2],['Sabedoria','sabedoria',2],['Carisma','carisma',2],
+				['Nome','nome',3], ['Classe','classe',3], ['Raça','raca',3], ['Nível','lv',1],
+				['Tipo','tipo',2], ['Xp','xp',3], 
+				['Iniciativa','iniciativa',2], ['Sentidos','sentidos',7], 
+				['PV','pv',2], ['Sangrando','sangrando',2], 
+				['CA','ca',2], ['Fortitude','fort',3], ['Reflexos','refle',3],['Vontade','vontade',3],
+				['Imunidades','imunidades',7], 
+				['Testes de Resistência','testes_de_resistencia',2],
+				['Deslocamento','deslocamento',9],
+				['Pontos de Ação', 'pontos_de_acao',3],
+				['Tendência','tendencia',4],
+				['Idiomas','idiomas',8],
+				['Perícias','periicias',12]
+				
 			];
-/*
- * a variavel $objeto e um array e $atributos[$i][1] e o indice
- * verificamos dentro de helper_check_value() se o array $objeto
- * contem um indice conforme passado por parametro. Caso nao tenha
- * este indice subentendemos que se trata do indice dados com uma 
- * string serializada. Apos isso damos um unserialize e retornamos
- * uma nova lista com os arquivos serializado.
- */
+
 for($i=0; $i<=count($atributos)-1; $i++):
-	$form->_col(2);
+	$form->_col($atributos[$i][2]);
 		$form->label($atributos[$i][0]);
-		$form->input(['name' => $atributos[$i][1], 'type' => 'text', 'class'=>'form-control', 'required'=>'false', 'value'=> helper_check_value($objeto[0], $atributos[$i][1])]);
-	$form->col_();
-endfor;
-
-$caracteristicas = [
-						['Nome','nome'], ['Raça','raca'], ['Classe','classe'], ['Caminho exemplar','caminho_exemplar'],
-						['Destino épico','destino_epico'], ['Tendencia','tendencia'], ['Nível','lv'], 
-						['Idade','idade'], ['Altura','altura'], ['Peso','peso'], ['Divindade','divindade'], ['Sexo','sexo'],
-						['Região de origem','regiao_origem'], ['Idiomas','idiomas']
-				   ];
-for($i=0; $i<=count($caracteristicas)-1; $i++):
-	$form->_col(3);
-		$form->label($caracteristicas[$i][0]);
-		if(!isset($caracteristicas[$i][2])):
-			$required = 'false';
-		else:
-			$required = 'false';
-		endif;
-		$form->input(['name' => $caracteristicas[$i][1], 'type' => 'text', 'class'=>'form-control', 'value'=> helper_check_value($objeto[0], $caracteristicas[$i][1])]);
-	$form->col_();
-endfor;
-
-$atributos_secundarios = [
-							['Iniciativa','iniciativa'], ['Deslocamento','deslocamento'], ['Intuição passiva','intuicao_passiva'],
-							['Percepção passiva','percepcao_passiva'],
-							['Pontos de vida','pv'], ['Sangrando','sangrando'], ['Pulso de cura','cura'], ['Pulsos','dia'], ['Pontos de ação','pts_acao'],
-							['Classe de armadura','ca'], ['Fortitude','fort'], ['Reflexos','refl'], ['Vontade','vont'],
-							['Bônus de ataque','ataque'], ['Bônus de dano','dano'], ['Pontos de experiência','xp']
-				   		];
-for($i=0; $i<=count($atributos_secundarios)-1; $i++):
-	$form->_col(2);
-		if(!isset($caracteristicas[$i][2])):
-			$required = 'false';
-		else:
-			$required = 'false';
-		endif;
-		$form->label($atributos_secundarios[$i][0]);
-		$form->input(['name' => $atributos_secundarios[$i][1], 'type' => 'text', 'class'=>'form-control', 'value'=> helper_check_value($objeto[0], $atributos_secundarios[$i][1])]);
+		$form->input(['name' => $atributos[$i][1], 'type' => 'text', 'class'=>'form-control', 'required' => 'true' , 'value'=> helper_check_value($objeto[0], $atributos[$i][1])]);
 	$form->col_();
 endfor;
 
 
-helper_form_input("Imagem", ['name' => 'img', 'type' => 'file', 'class'=>'form-control'], 10);
+helper_form_input("Imagem", ['name' => 'img', 'type' => 'file', 'class'=>'form-control'], 12);
 
-helper_form_text_area("Ataques básicos", ['name' => 'ataque', 'class'=>'form-control', 'rows'=>'5'], helper_check_value($objeto[0], 'ataque'), 6);
+helper_form_text_area("Descrição", ['name' => 'descricao', 'class'=>'form-control', 'rows'=>'5'], helper_check_value($objeto[0], 'descricao'), 6);
 
-helper_form_text_area("Aspectos raciais", ['name' => 'aspectos_raciais', 'class'=>'form-control', 'rows'=>'5'], helper_check_value($objeto[0], 'aspectos_raciais'), 6);
+helper_form_text_area("Ataque", ['name' => 'ataque', 'class'=>'form-control', 'rows'=>'5'], helper_check_value($objeto[0], 'ataque'), 6);
 
-helper_form_text_area("Características de classe/trilha/destino", ['name' => 'caracteristicas', 'class'=>'form-control', 'rows'=>'5'], helper_check_value($objeto[0], 'caracteristicas'), 6);
+helper_form_text_area("Habilidades", ['name' => 'habilidades', 'class'=>'form-control', 'rows'=>'5'], helper_check_value($objeto[0], 'habilidades'), 6);
 
-helper_form_text_area("Talentos", ['name' => 'talentos', 'class'=>'form-control', 'rows'=>'5'], helper_check_value($objeto[0], 'talentos'), 6);
-
-helper_form_text_area("Perícias", ['name' => 'pericias', 'class'=>'form-control', 'rows'=>'5'], helper_check_value($objeto[0], 'pericias'), 6);
-
-helper_form_text_area("Poderes", ['name' => 'poderes', 'class'=>'form-control', 'rows'=>'5'], helper_check_value($objeto[0], 'poderes'), 6);
-
-helper_form_text_area("Itens e equipamentos", ['name' => 'equipamentos', 'class'=>'form-control', 'rows'=>'5'], helper_check_value($objeto[0], 'equipamentos'), 6);
-
-helper_form_text_area("Habilidades especiais", ['name' => 'habilidades_especiais', 'class'=>'form-control', 'rows'=>'5'], helper_check_value($objeto[0], 'habilidades_especiais'), 6);
-
-helper_form_text_area("Costumes e aparência", ['name' => 'costumes', 'class'=>'form-control', 'rows'=>'5'], helper_check_value($objeto[0], 'costumes'), 6);
-
-helper_form_text_area("Traços e personalidades", ['name' => 'tracos', 'class'=>'form-control', 'rows'=>'5'], helper_check_value($objeto[0], 'tracos'), 6);
-
-helper_form_text_area("Antecedentes", ['name' => 'antecedentes', 'class'=>'form-control', 'rows'=>'5'], helper_check_value($objeto[0], 'antecedentes'), 6);
-
-helper_form_text_area("Outros", ['name' => 'outros', 'class'=>'form-control', 'rows'=>'5'], helper_check_value($objeto[0], 'outros'), 6);
+helper_form_text_area("Táticas", ['name' => 'taticas', 'class'=>'form-control', 'rows'=>'5'], helper_check_value($objeto[0], 'taticas'), 6);
