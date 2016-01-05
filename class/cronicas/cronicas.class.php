84afcb9a80077c93c78865356c4e252d6e18a9cc
maickon <maickonmaickon@hotmail.com>
2016-01-05 00:41:48 -0200

    nova classe para criar cronicas de RPG

---
+++ class/cronicas/cronicas.class.php
@@ -0,0 +1,21 @@
+<?php
+class Cronicas extends Db_persistence{
+	private $attr;
+	
+	function __construct($path = "", $warning_msg = 'Já existe uma crônica com este nome!'){
+		parent::__construct(strtolower(get_class($this)), $path, $warning_msg);
+	}
+	
+	function __set($attr_name, $attr_value){
+	
+		$this->attr["{$attr_name}"] = $attr_value;
+	}
+	
+	function __get($attr){
+		return $this->attr["{$attr}"];
+	}
+	
+	function getTable(){
+		return $this->table;
+	}
+}
