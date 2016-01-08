d7e5a16d6233141ac5ff8a18dc1921802741408c
maickon <maickonmaickon@hotmail.com>
2016-01-08 01:08:30 -0200

    emcapsulando o codigo dos anuncios em helpers

--- pages/login/index.php
+++ pages/login/index.php
@@ -3,19 +3,7 @@ require_once '../../header.php';
 
 $form->_container();
 	$form->_col(12);
-		$tag->imprime('
-					<div class="banner-center">
-						<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-						<!-- anuncio_1 -->
-						<ins class="adsbygoogle"
-						     style="display:inline-block;width:728px;height:90px"
-						     data-ad-client="ca-pub-3010334569259161"
-						     data-ad-slot="8073846133"></ins>
-						<script>
-						(adsbygoogle = window.adsbygoogle || []).push({});
-						</script>
-					</div>
-					');
+		helper_adsense_01();
 	$form->col_();
 $form->container_();
 
