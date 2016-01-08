7d7e4872e4716374ddef8ff10a37feae6e996e63
maickon <maickonmaickon@hotmail.com>
2016-01-08 01:14:20 -0200

    melhorando pagina de visualizacao da ficha de D&D 4.0

--- pages/painel/monstros/views/d20-ded-4.0.php
+++ pages/painel/monstros/views/d20-ded-4.0.php
@@ -19,12 +19,12 @@ function helper_show_monstro_ded4_0($personagem){
 			$form->col_();
 			$form->_col(6);
 				$tag->imprime($form->bold($personagem['raca'].' '.$personagem['classe']));	
-				$tag->br();
-				$tag->imprime('('.$unserialize_params['tipo_monstro'].')');
+				$tag->imprime($unserialize_params['tipo_monstro']);
 			$form->col_();
 			$form->_col(6);
 				$tag->imprime($form->bold('Personagem de Nível '.$personagem['lv']));
-				$tag->br();
+			$form->col_();
+			$form->_col(6);
 				$tag->imprime(XP.' '.$unserialize_params['xp']);
 			$form->col_();
 		$form->row_();
@@ -32,15 +32,14 @@ function helper_show_monstro_ded4_0($personagem){
 	$tag->div('class="col-md-12 body1_ded_4-0"');
 		$form->_row();
 			$form->_col(6);
-				$tag->imprime($form->bold(INICIATIVA).' '.helper_check_params('iniciativa',$unserialize_params));
+				$tag->imprime($form->bold(SENTIDOS).' '.$unserialize_params['sentidos']);
 			$form->col_();
-			
 			$form->_col(6);
-				$tag->imprime($form->bold(SENTIDOS).' '.$unserialize_params['sentidos']);	
+				$tag->imprime($form->bold(INICIATIVA).' '.helper_check_params('iniciativa',$unserialize_params));
 			$form->col_();
 		$form->row_();
 	$tag->div;	
-	$tag->div('class="col-md-12 body1_ded_4-0"');
+	$tag->div('class="col-md-12 body2_ded_4-0"');
 		$form->_row();
 			$form->_col(6);
 				$tag->imprime($form->bold(PONTOS_VIDA).' '.$unserialize_params['pv']);
@@ -53,8 +52,10 @@ function helper_show_monstro_ded4_0($personagem){
 	
 	$tag->div('class="col-md-12 body1_ded_4-0"');
 		$form->_row();
-			$form->_col(12);
+			$form->_col(6);
 				$tag->imprime($form->bold(CA).' '.$unserialize_params['ca'].';');
+			$form->col_();
+			$form->_col(6);
 				$tag->imprime($form->bold(FORTITUDE).' '.$unserialize_params['fort'].';');
 				$tag->imprime($form->bold(VONTADE).' '.$unserialize_params['vont'].';');
 				$tag->imprime($form->bold(REFLEXO).' '.$unserialize_params['refl'].';');
@@ -62,7 +63,7 @@ function helper_show_monstro_ded4_0($personagem){
 		$form->row_();
 	$tag->div;	
 	
-	$tag->div('class="col-md-12 body1_ded_4-0"');
+	$tag->div('class="col-md-12 body2_ded_4-0"');
 		$form->_row();
 			$form->_col(6);
 				$tag->imprime($form->bold(DESL).' '.$unserialize_params['deslocamento'].';');
@@ -73,7 +74,7 @@ function helper_show_monstro_ded4_0($personagem){
 		$form->row_();
 	$tag->div;	
 	
-	$tag->div('class="col-md-12 body2_ded_4-0"');
+	$tag->div('class="col-md-12 body1_ded_4-0"');
 		$form->_row();
 			$form->_col(6);
 				$tag->imprime($form->bold(TESTES_RESISTENCIA).' '.helper_check_params('testes_de_resistencia',$unserialize_params).';');
@@ -96,22 +97,22 @@ function helper_show_monstro_ded4_0($personagem){
 	$tag->div('class="col-md-12 body2_ded_4-0"');
 		$form->_row();
 			$form->_col(4);
-				$tag->imprime($form->bold(FORCA)."{$unserialize_params['forca']}".' ('.mod($unserialize_params['forca']).')');
+				$tag->imprime($form->bold(FORCA)." {$unserialize_params['forca']}");
 			$form->col_();
 			$form->_col(4);
-				$tag->imprime($form->bold(CON)."{$unserialize_params['constituicao']}".' ('.mod($unserialize_params['constituicao']).')');
+				$tag->imprime($form->bold(CON)." {$unserialize_params['constituicao']}");
 			$form->col_();
 			$form->_col(4);
-				$tag->imprime($form->bold(DES)."{$unserialize_params['destreza']}".' ('.mod($unserialize_params['destreza']).')');
+				$tag->imprime($form->bold(DES)." {$unserialize_params['destreza']}");
 			$form->col_();
 			$form->_col(4);
-				$tag->imprime($form->bold(INTELIGENCIA)."{$unserialize_params['inteligencia']}".' ('.mod($unserialize_params['inteligencia']).')');
+				$tag->imprime($form->bold(INTELIGENCIA)." {$unserialize_params['inteligencia']}");
 			$form->col_();
 			$form->_col(4);
-				$tag->imprime($form->bold(SAB)."{$unserialize_params['sabedoria']}".' ('.mod($unserialize_params['sabedoria']).')');
+				$tag->imprime($form->bold(SAB)." {$unserialize_params['sabedoria']}");
 			$form->col_();
 			$form->_col(4);
-				$tag->imprime($form->bold(CAR)."{$unserialize_params['carisma']}".' ('.mod($unserialize_params['carisma']).')');
+				$tag->imprime($form->bold(CAR)." {$unserialize_params['carisma']}");
 			$form->col_();
 		$form->row_();
 	$tag->div;
@@ -121,18 +122,27 @@ function helper_show_monstro_ded4_0($personagem){
 			$form->_col(12);
 				$tag->imprime($form->bold(DESCRICAO).'<br>'.helper_check_params('descricao',$unserialize_params));
 			$form->col_();
+		$form->row_();
+	$tag->div;
+			
+	$tag->div('class="col-md-12 body2_ded_4-0"');
+		$form->_row();
 			$form->_col(12);
 				$tag->imprime($form->bold(ATAQUE).'<br>'.helper_check_params('ataque',$unserialize_params));
 			$form->col_();
 		$form->row_();
 	$tag->div;
 	
-	$tag->div('class="col-md-12 body2_ded_4-0 border-button"');
+	$tag->div('class="col-md-12 body1_ded_4-0"');
 		$form->_row();
 			$form->_col(12);
 				$tag->imprime($form->bold(HABILIDADES).'<br>'.helper_check_params('habilidades',$unserialize_params));
 			$form->col_();
+		$form->row_();
+	$tag->div;
 			
+	$tag->div('class="col-md-12 body2_ded_4-0 border-button"');
+		$form->_row();
 			$form->_col(12);
 				$tag->imprime($form->bold('Táticas').'<br>'.helper_check_params('taticas',$unserialize_params));
 			$form->col_();
