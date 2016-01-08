dfeeb327fca99d6c92f6c6a2d063d15670db9430
maickon <maickonmaickon@hotmail.com>
2016-01-08 01:19:24 -0200

    novo arquivo de rotas para montar os caminhos dentro da pagina administrativa

---
+++ rotas/rotas-painel.php
@@ -0,0 +1,33 @@
+<?php
+define('ARMASPATH', "pages/painel/armas/");
+define('ARMASIMGPATH', "img/armas/");
+
+define('ARMADURASPATH' , "pages/painel/armaduras/");
+define('ARMADURASIMGPATH', "img/armaduras/");
+
+define('ESCUDOSPATH' , "pages/painel/escudos/");
+
+define('ARTEFATOSPATH', "pages/painel/artefatos/");
+define('ARTEFATOSIMGPATH', "img/artefatos/");
+
+define('TALENTOSPATH', "pages/painel/talentos/");
+define('TALENTOSIMGPATH', "img/talentos/");
+
+define('MAGIASPATH', "pages/painel/magias/");
+define('MAGIASIMGPATH', "img/magias/");
+
+define('PERICIASPATH', "pages/painel/pericias/");
+define('PERICIASIMGPATH', "img/pericias/");
+
+define('PERSONAGEMPATH', "pages/painel/personagens/");
+define('PERSONAGEMIMGPATH', "img/personagens/");
+
+define('MONSTROPATH', "pages/painel/monstros/");
+define('MONSTROIMGPATH', "img/monstros/");
+
+define('BOSSPATH', "pages/painel/boss/");
+define('BOSSIMGPATH', "img/boss/");
+
+define('HABILIDADESPATH', "pages/painel/habilidades");
+
+//define('HISTORIASPATH', "pages/painel/historias");
