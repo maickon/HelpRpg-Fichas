e0d02d8c7cd2e084d14ca05d2e27c5fcc774f9cd
maickon <maickonmaickon@hotmail.com>
2016-01-08 01:12:17 -0200

    melhorando o formulario do RPG sevage worlds

--- pages/painel/monstros/form/savage_worlds.php
+++ pages/painel/monstros/form/savage_worlds.php
@@ -5,21 +5,35 @@ $form->input(['name' => 'dono', 'type' => 'hidden', 'value'=> $current_user]);
 $form->input(['name' => 'sistema', 'type' => 'hidden', 'value'=> 'Savage Worlds']);
 $form->input(['name' => 'tipo', 'type' => 'hidden', 'value'=> 'Monstro']);
 
-helper_form_select_options("Força", [ 'class'=>'form-control'], ['d4','d6','d8','d10','d12'], 3);
+function helper_form_sevage($array_a_ser_mesclado){
+	$opcoes_valores_atributos = ['d4','d6','d8','d10','d12'];
+	
+	if(isset($array_a_ser_mesclado)):
+		$novo_array = array_merge($array_a_ser_mesclado, $opcoes_valores_atributos);
+		return $novo_array;
+	else:
+		return $opcoes_valores_atributos;
+	endif;
+}
 
-helper_form_select_options("Agilidade", [ 'class'=>'form-control'], ['d4','d6','d8','d10','d12'], 2);
+helper_form_select_options("Força", [ 'class'=>'form-control', 'name' => 'forca'], helper_form_sevage([helper_check_value($objeto[0],'forca')]), 3);
 
-helper_form_select_options("Astúcia", [ 'class'=>'form-control'], ['d4','d6','d8','d10','d12'], 2);
+helper_form_select_options("Agilidade", [ 'class'=>'form-control', 'name' => 'agilidade'], helper_form_sevage([helper_check_value($objeto[0],'agilidade')]), 2);
 
-helper_form_select_options("Espírito", [ 'class'=>'form-control'], ['d4','d6','d8','d10','d12'], 2);
+helper_form_select_options("Astúcia", [ 'class'=>'form-control', 'name' => 'astucia'], helper_form_sevage([helper_check_value($objeto[0],'astucia')]), 2);
 
-helper_form_select_options("Vigor", [ 'class'=>'form-control'], ['d4','d6','d8','d10','d12'], 3);
+helper_form_select_options("Espírito", [ 'class'=>'form-control', 'name' => 'espirito'], helper_form_sevage([helper_check_value($objeto[0],'espirito')]), 2);
+
+helper_form_select_options("Vigor", [ 'class'=>'form-control', 'name' => 'vigor'], helper_form_sevage([helper_check_value($objeto[0],'vigor')]), 3);
 
 $atributos = [
-				['Nome','nome',3],
+				[NOME,'nome',3],
 				['Aparar','aparar',2],['Movimentação','movimentacao',2],['Resistência','resistencia',2],
 				['Carisma','carisma',3],
-				['Perícias','pericias',12]
+				['Perícias','pericias',12],
+				[NIVEL,'lv',2],
+				[CLASSE,'classe',3],
+				[RACA,'raca',3]
 				
 			];
 
@@ -30,7 +44,7 @@ for($i=0; $i<=count($atributos)-1; $i++):
 	$form->col_();
 endfor;
 
-helper_form_input("Imagem", ['name' => 'img', 'type' => 'file', 'class'=>'form-control'], 12);
+helper_form_input("Imagem", ['name' => 'img', 'type' => 'file', 'class'=>'form-control'], 4);
 
 
 helper_form_text_area("Descrição", ['name' => 'descricao', 'class'=>'form-control', 'rows'=>'5'], helper_check_value($objeto[0], 'descricao'), 6);
