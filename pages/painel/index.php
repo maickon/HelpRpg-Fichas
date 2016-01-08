5a9e73aaf445b86c41dab067e62da4ca2e362e74
maickon <maickonmaickon@hotmail.com>
2016-01-08 14:41:05 -0200

    exibindo novas funcoes no painel

--- pages/painel/index.php
+++ pages/painel/index.php
@@ -27,24 +27,39 @@ $form->_row();
 		$armadura 	= new Armaduras('');
 		$arma 		= new Armas('');
 		$artefato 	= new Artefatos('');
+		$historias 	= new Historias();
+		$contos 	= new Contos();
+		$cronicas 	= new Cronicas();
+		$aventuras 	= new Aventuras();
 		
 		$number_user 		= $user->select('usuarios');
 		$number_armaduras 	= $armadura->select('armaduras');
 		$number_armas 		= $arma->select('armas');
 		$number_artefatos 	= $artefato->select('artefatos');
+		$number_contos 		= $contos->select('contos');
+		$number_cronicas 	= $cronicas->select('cronicas');
+		$number_aventuras 	= $aventuras->select('aventuras');
 		
 		$qtd_user 		= count($number_user);
 		$qtd_armaduras 	= count($number_armaduras);
 		$qtd_armas 		= count($number_armas);
 		$qtd_artefatos 	= count($number_artefatos);
+		$qtd_aventuras 	= count($number_aventuras);
+		$qtd_contos 	= count($number_contos);
+		$qtd_cronicas 	= count($number_cronicas);
 		
+		$qtd_total_utilitarios = 5;
 		$qtd_total_itens = ($qtd_armaduras+$qtd_armas+$qtd_artefatos);
 		
 		$img_title = [
-						['jogadores.jpg','Usuários Cadastrados', $qtd_user],
-						['monstros.jpg','Monstros cadastrados', 0],
-						['boss.jpg','Chefes de fase', 0],
-						['armas.jpg','Itens Cadastrados', $qtd_total_itens]
+						['jogadores.jpg',USUARIOS_CADASTRADOS, $qtd_user],
+						['monstros.jpg',MONSTROS_CADASTRADOS, 0],
+						['boss.jpg',CHEFES_DE_FASE, 0],
+						['armas.jpg',ITENS_CADASTRADOS, $qtd_total_itens],
+						['aventuras.jpg',AVENTURAS, $qtd_aventuras],
+						['contos.jpg',CONTOS, $qtd_contos],
+						['cronicas.jpg',CRONICAS, $qtd_cronicas],
+						['utilitarios.jpg',UTILITARIOS, $qtd_total_utilitarios]
 					];
 		
 		for($i=0; $i<count($img_title); $i++):
