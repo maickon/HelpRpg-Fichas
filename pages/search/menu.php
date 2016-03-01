a9286bf3041b9c790bfc096d647b969c58c0c5f3
maickon <maickonmaickon@hotmail.com>
2016-03-01 11:40:22 -0300

    add arquivo de menu separado

---
+++ pages/search/menu.php
@@ -0,0 +1,3 @@
+<?php
+$nomes = array('Home','Cadastro','Sobre','Rola dados','Gerador de NPCs','Gerador de Monstros','Como Usar');
+$links = array(ROOTPATHURL, USERNEWPATH, ABOUTPATH, ROLLDICE, NPCGENERATEPATH, MONSTERGENERATEPATH, HOWTOUSE);
