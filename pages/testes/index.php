efdc0c387df25e4e4f6356fa21c66dd0a541579d
maickon <maickonmaickon@hotmail.com>
2016-03-01 00:37:46 -0300

    pagina principal para fazer pesquisas no site

--- pages/testes/index.php
+++ pages/testes/index.php
@@ -1,12 +1,9 @@
 <?php
 require_once '../../init.php';
 require_once '../helper.php';
+require_once 'menu.php';
 
 global $tag, $form;
-
-$nomes = array('Home','Cadastro','Sobre','Rola dados','Gerador de NPCs','Gerador de Monstros','Como Usar');
-$links = array(ROOTPATHURL.BASE_PATH, ROOTPATHURL.USERNEWPATH, ROOTPATHURL.ABOUTPATH, ROLLDICE, NPCGENERATEPATH, MONSTERGENERATEPATH, ROOTPATHURL.HOWTOUSE);
-
 
 $tag->html('lang="pt-br"');
 	$tag->head();
@@ -20,94 +17,51 @@ $tag->html('lang="pt-br"');
 		$tag->imprime('<link rel="shortcut icon" href="'.ROOTPATHURL.IMGPATH.'logo-icon.png" />');
 		$tag->link('rel="icon" href="'.IMGPATH.'adm.png"');
 		
+
 		//Titulo do site
 		$tag->title();
 			$tag->imprime(PROJECTTITLE);
 		$tag->title;
 		
-		//<!-- datatable css -->
-		$tag->link('href="'.ROOTPATHURL.CSSPATH.'datatables.min.css" rel="stylesheet"');
-		
-		//<!-- Bootstrap core CSS -->
-		$tag->link('href="'.ROOTPATHURL.CSSPATH.'bootstrap.min.css" rel="stylesheet"');
-  		
+		$tag->link('href="'.TESTEPATH.CSSPATH.'index.css" rel="stylesheet"');
 		//<!-- index core CSS -->
 		$tag->link('href="'.ROOTPATHURL.CSSPATH.'index.css" rel="stylesheet"');
 		
-		//<!-- selec core CSS -->
-		$tag->link('href="'.ROOTPATHURL.CSSPATH.'bootstrap-select.css" rel="stylesheet"');
+		//<!-- Bootstrap core CSS -->
+		$tag->link('href="'.ROOTPATHURL.CSSPATH.'bootstrap.min.css" rel="stylesheet"');
 		
+		//<!-- jquery CSS -->
+		$tag->link('href="'.TESTEPATH.CSSPATH.'jquery-ui.css" rel="stylesheet"');
+
 		$tag->script('src="'.ROOTPATHURL.JSPATH.'jquery.min.js"');
-		$tag->script;
-		
-		$tag->script('src="'.ROOTPATHURL.JSPATH.'datatables.min.js"');
-		$tag->script;
-		
-		$tag->script('src="'.ROOTPATHURL.JSPATH.'bootstrap-select.js"');
-		$tag->script;
-		
-		$tag->script('src="'.ROOTPATHURL.JSPATH.'bibliotecaAjax.js"');
 		$tag->script;
 
 	$tag->head;
 $tag->body();
 	
-	$tag->form('action="javascript:void%200" onSubmit="buscar_classe(this.search.value); return false" ');
+	$tag->form('action="search.php" method="post"');
 		$tag->div('class="container search_box" id="search_box"');	
 			$form->_row();
-				$form->_col(3);
+				$form->_col(2);
 				$form->col_();
 				
-				$form->_col(6);	
+				$form->_col(8);	
 					$tag->div('class="logo"');
 						$tag->img('src="'.ROOTPATHURL.IMGPATH.'logo.png" class="logo"');
 					$tag->div;
-					$form->input(['type'=>'text', 'id'=>'search' ,'class'=>'form-control input-lg', 'aria-describedby'=>'basic-addon1','placeholder'=>'Busque: classe, raça, nome, dono da ficha, ficha tipo, sistema de rpg ou Lv']);	
+					$tag->div('class="ui-widget"');
+						$form->input(['type'=>'text', 'name'=>'search', 'id'=>'search' ,'class'=>'form-control input-lg', 'aria-describedby'=>'basic-addon1','placeholder'=>'Busque: classe, raça, nome, dono da ficha, ficha tipo, sistema de rpg ou Lv']);	
+					$tag->div;
 				$form->col_();	
 			$form->row_();
 		$form->container_();
 	$tag->form;
 	
-	$tag->div('class="container" id="box-result"');
-		$form->_row();
-			$form->_col(6);
-				$tag->form('action="javascript:void%200" onchange="buscar_classe(this.search_result.value); return false" ');
-					$form->h2("Nova busca...");
-					$form->input(['type'=>'text', 'id'=>'search_result' ,'class'=>'form-control input-lg', 'aria-describedby'=>'basic-addon1','placeholder'=>'Digite sua busca. Ex: Espada Larga']);
-				$tag->form;
-			$form->col_();
-			
-			$form->_col(6);
-				$form->h2("Publicidade");
-				$tag->div('class="propaganda"');
-				
-				$tag->div;
-				$tag->br();
-			$form->col_();
+	require 'footer.php';
 
-			$form->_col(6);
-				$tag->span('id="classe_personagem"');
-				$tag->span;
-			$form->col_();
-			
-			$tag->div('id="image"');
-				
-			$tag->div;
-			
-		$form->row_();
-	$tag->div;
-	
-	$tag->div('class="container"');
-		$tag->div('class="rodape-search-page"');
-			helper_footer_bar_page_search($links, $nomes);
-		$tag->div;
-		$tag->div('class="copy"');
-			$tag->imprime("Desenvolvimento Equipe Help RPG - Todos os direitos reservados");
-			$tag->br();
-			$tag->imprime("2013-2015");
-		$tag->div;
-	$tag->div;
-	
-	$tag->script('src="'.ROOTPATHURL.JSPATH.'home.js"');
+	$tag->script('src="'.TESTEPATH.JSPATH.'jquery-ui.js"');
+	$tag->script;
+
+	$tag->script('src="'.TESTEPATH.JSPATH.'index.js"'); 
 	$tag->script;
 $tag->body;
