bf9cdc2df8534ef962a957c82a25d84f33a9531a
maickon <maickonmaickon@hotmail.com>
2016-03-01 11:39:07 -0300

    arquivos deletados

--- pages/testes/menu.php
+++
@@ -1,3 +0,0 @@
-<?php
-$nomes = array('Home','Cadastro','Sobre','Rola dados','Gerador de NPCs','Gerador de Monstros','Como Usar');
-$links = array(ROOTPATHURL.BASE_PATH, ROOTPATHURL.USERNEWPATH, ROOTPATHURL.ABOUTPATH, ROLLDICE, NPCGENERATEPATH, MONSTERGENERATEPATH, ROOTPATHURL.HOWTOUSE);
