9876621840914b6ef62e0e437248ed2b56979cc1
maickon <maickonmaickon@hotmail.com>
2016-03-02 00:02:10 -0300

    alteracao no menu

--- pages/search/menu.php
+++ pages/search/menu.php
@@ -1,3 +1,3 @@
 <?php
 $nomes = array('Home','Cadastro','Sobre','Rola dados','Gerador de NPCs','Gerador de Monstros','Como Usar');
-$links = array(ROOTPATHURL, USERNEWPATH, ABOUTPATH, ROLLDICE, NPCGENERATEPATH, MONSTERGENERATEPATH, HOWTOUSE);
+$links = array(HOME_PATH, USERNEWPATH, ABOUTPATH, ROLLDICE, NPCGENERATEPATH, MONSTERGENERATEPATH, HOWTOUSE);
