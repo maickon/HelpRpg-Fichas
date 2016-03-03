f556bc55e63867161be7425dfa5b4b4a4bf97d64
maickon <maickonmaickon@hotmail.com>
2016-03-03 00:10:45 -0300

    criando as proprias funcoes de exibicao dos dados pesquisados

--- pages/search/results/results.php
+++ pages/search/results/results.php
@@ -60,3 +60,311 @@ function armadura($filter_result){
 		$tag->td;
 	$tag->tr;
 }
+
+function arma($filter_result){
+	global $tag;
+	$path = ARMASPATH;
+	$tag->tr();
+		$tag->td();
+			$tag->span('class="search-title"');
+				$tag->a('href="'.ROOTPATHURL.$path.'view.php?id='.$filter_result['id'].'" target="blank"');
+					$tag->imprime($filter_result['nome']);
+				$tag->a;
+			$tag->span;
+			$tag->br();
+			$tag->imprime("Dano {$filter_result['dano']} - Nível {$filter_result['lv']}, Sitema {$filter_result['sistema']}");
+			$tag->br();
+			$tag->imprime("Criado por {$filter_result['dono']}");
+			$tag->br();
+			$tag->imprime("<i>{$filter_result['descricao']}</i>");
+			$tag->br();
+			$tag->small();
+				$tag->a('href="'.ROOTPATHURL.$path.'view.php?id='.$filter_result['id'].'" target="blank" class="show-picture"');
+					$tag->imprime('Ver mais...');
+				$tag->a;
+			$tag->small;
+			$tag->br();
+			$tag->br();
+		$tag->td;
+	$tag->tr;
+}
+
+function artefato($filter_result){
+	global $tag;
+	$path = ARTEFATOSPATH;
+	$tag->tr();
+		$tag->td();
+			$tag->span('class="search-title"');
+				$tag->a('href="'.ROOTPATHURL.$path.'view.php?id='.$filter_result['id'].'" target="blank"');
+					$tag->imprime($filter_result['nome']);
+				$tag->a;
+			$tag->span;
+			$tag->br();
+			$tag->imprime("Nível {$filter_result['lv']}, Sitema {$filter_result['sistema']}");
+			$tag->br();
+			$tag->imprime("Criado por {$filter_result['dono']}");
+			$tag->br();
+			$tag->imprime("<i>{$filter_result['descricao']}</i>");
+			$tag->br();
+			$tag->small();
+				$tag->a('href="'.ROOTPATHURL.$path.'view.php?id='.$filter_result['id'].'" target="blank" class="show-picture"');
+					$tag->imprime('Ver mais...');
+				$tag->a;
+			$tag->small;
+			$tag->br();
+			$tag->br();
+		$tag->td;
+	$tag->tr;
+}
+
+function talento($filter_result){
+	global $tag;
+	$path = TALENTOSPATH;
+	$tag->tr();
+		$tag->td();
+			$tag->span('class="search-title"');
+				$tag->a('href="'.ROOTPATHURL.$path.'view.php?id='.$filter_result['id'].'" target="blank"');
+					$tag->imprime($filter_result['nome']);
+				$tag->a;
+			$tag->span;
+			$tag->br();
+			$tag->imprime("Nível {$filter_result['lv']}, Sitema {$filter_result['sistema']}");
+			$tag->br();
+			$tag->imprime("Criado por {$filter_result['dono']}");
+			$tag->br();
+			$tag->imprime("<i>{$filter_result['descricao']}</i>");
+			$tag->br();
+			$tag->small();
+				$tag->a('href="'.ROOTPATHURL.$path.'view.php?id='.$filter_result['id'].'" target="blank" class="show-picture"');
+					$tag->imprime('Ver mais...');
+				$tag->a;
+			$tag->small;
+			$tag->br();
+			$tag->br();
+		$tag->td;
+	$tag->tr;
+}
+
+function magia($filter_result){
+	global $tag;
+	$path = MAGIASPATH;
+	$tag->tr();
+		$tag->td();
+			$tag->span('class="search-title"');
+				$tag->a('href="'.ROOTPATHURL.$path.'view.php?id='.$filter_result['id'].'" target="blank"');
+					$tag->imprime($filter_result['nome']);
+				$tag->a;
+			$tag->span;
+			$tag->br();
+			$tag->imprime("Nível {$filter_result['lv']}, Sitema {$filter_result['sistema']}");
+			$tag->br();
+			$tag->imprime("Criado por {$filter_result['dono']}");
+			$tag->br();
+			$tag->imprime("<i>{$filter_result['descricao']}</i>");
+			$tag->br();
+			$tag->small();
+				$tag->a('href="'.ROOTPATHURL.$path.'view.php?id='.$filter_result['id'].'" target="blank" class="show-picture"');
+					$tag->imprime('Ver mais...');
+				$tag->a;
+			$tag->small;
+			$tag->br();
+			$tag->br();
+		$tag->td;
+	$tag->tr;
+}
+
+function pericia($filter_result){
+	global $tag;
+	$path = PERICIASPATH;
+	$tag->tr();
+		$tag->td();
+			$tag->span('class="search-title"');
+				$tag->a('href="'.ROOTPATHURL.$path.'view.php?id='.$filter_result['id'].'" target="blank"');
+					$tag->imprime($filter_result['nome']);
+				$tag->a;
+			$tag->span;
+			$tag->br();
+			$tag->imprime("Nível {$filter_result['lv']}, Sitema {$filter_result['sistema']}");
+			$tag->br();
+			$tag->imprime("Criado por {$filter_result['dono']}");
+			$tag->br();
+			$tag->imprime("<i>{$filter_result['descricao']}</i>");
+			$tag->br();
+			$tag->small();
+				$tag->a('href="'.ROOTPATHURL.$path.'view.php?id='.$filter_result['id'].'" target="blank" class="show-picture"');
+					$tag->imprime('Ver mais...');
+				$tag->a;
+			$tag->small;
+			$tag->br();
+			$tag->br();
+		$tag->td;
+	$tag->tr;
+}
+
+function aventura($filter_result){
+	global $tag;
+	$path = AVENTURAPATH;
+	$tag->tr();
+		$tag->td();
+			$tag->span('class="search-title"');
+				$tag->a('href="'.ROOTPATHURL.$path.'view.php?id='.$filter_result['id'].'" target="blank"');
+					$tag->imprime($filter_result['nome']);
+				$tag->a;
+			$tag->span;
+			$tag->br();
+			$tag->imprime("Nível {$filter_result['lv']}, Sitema {$filter_result['sistema']}");
+			$tag->br();
+			$tag->imprime("Criado por {$filter_result['dono']}");
+			$tag->br();
+			$tag->imprime("<i>{$filter_result['descricao']}</i>");
+			$tag->br();
+			$tag->small();
+				$tag->a('href="'.ROOTPATHURL.$path.'view.php?id='.$filter_result['id'].'" target="blank" class="show-picture"');
+					$tag->imprime('Ver mais...');
+				$tag->a;
+			$tag->small;
+			$tag->br();
+			$tag->br();
+		$tag->td;
+	$tag->tr;
+}
+
+function historia($filter_result){
+	global $tag;
+	$path = ARTEFATOSPATH;
+	$tag->tr();
+		$tag->td();
+			$tag->span('class="search-title"');
+				$tag->a('href="'.ROOTPATHURL.$path.'view.php?id='.$filter_result['id'].'" target="blank"');
+					$tag->imprime($filter_result['nome']);
+				$tag->a;
+			$tag->span;
+			$tag->br();
+			$tag->imprime("Nível {$filter_result['lv']}, Sitema {$filter_result['sistema']}");
+			$tag->br();
+			$tag->imprime("Criado por {$filter_result['dono']}");
+			$tag->br();
+			$tag->imprime("<i>{$filter_result['descricao']}</i>");
+			$tag->br();
+			$tag->small();
+				$tag->a('href="'.ROOTPATHURL.$path.'view.php?id='.$filter_result['id'].'" target="blank" class="show-picture"');
+					$tag->imprime('Ver mais...');
+				$tag->a;
+			$tag->small;
+			$tag->br();
+			$tag->br();
+		$tag->td;
+	$tag->tr;
+}
+
+function conto($filter_result){
+	global $tag;
+	$path = ARTEFATOSPATH;
+	$tag->tr();
+		$tag->td();
+			$tag->span('class="search-title"');
+				$tag->a('href="'.ROOTPATHURL.$path.'view.php?id='.$filter_result['id'].'" target="blank"');
+					$tag->imprime($filter_result['nome']);
+				$tag->a;
+			$tag->span;
+			$tag->br();
+			$tag->imprime("Nível {$filter_result['lv']}, Sitema {$filter_result['sistema']}");
+			$tag->br();
+			$tag->imprime("Criado por {$filter_result['dono']}");
+			$tag->br();
+			$tag->imprime("<i>{$filter_result['descricao']}</i>");
+			$tag->br();
+			$tag->small();
+				$tag->a('href="'.ROOTPATHURL.$path.'view.php?id='.$filter_result['id'].'" target="blank" class="show-picture"');
+					$tag->imprime('Ver mais...');
+				$tag->a;
+			$tag->small;
+			$tag->br();
+			$tag->br();
+		$tag->td;
+	$tag->tr;
+}
+
+function cronica($filter_result){
+	global $tag;
+	$path = ARTEFATOSPATH;
+	$tag->tr();
+		$tag->td();
+			$tag->span('class="search-title"');
+				$tag->a('href="'.ROOTPATHURL.$path.'view.php?id='.$filter_result['id'].'" target="blank"');
+					$tag->imprime($filter_result['nome']);
+				$tag->a;
+			$tag->span;
+			$tag->br();
+			$tag->imprime("Nível {$filter_result['lv']}, Sitema {$filter_result['sistema']}");
+			$tag->br();
+			$tag->imprime("Criado por {$filter_result['dono']}");
+			$tag->br();
+			$tag->imprime("<i>{$filter_result['descricao']}</i>");
+			$tag->br();
+			$tag->small();
+				$tag->a('href="'.ROOTPATHURL.$path.'view.php?id='.$filter_result['id'].'" target="blank" class="show-picture"');
+					$tag->imprime('Ver mais...');
+				$tag->a;
+			$tag->small;
+			$tag->br();
+			$tag->br();
+		$tag->td;
+	$tag->tr;
+}
+
+function cenario($filter_result){
+	global $tag;
+	$path = ARTEFATOSPATH;
+	$tag->tr();
+		$tag->td();
+			$tag->span('class="search-title"');
+				$tag->a('href="'.ROOTPATHURL.$path.'view.php?id='.$filter_result['id'].'" target="blank"');
+					$tag->imprime($filter_result['nome']);
+				$tag->a;
+			$tag->span;
+			$tag->br();
+			$tag->imprime("Nível {$filter_result['lv']}, Sitema {$filter_result['sistema']}");
+			$tag->br();
+			$tag->imprime("Criado por {$filter_result['dono']}");
+			$tag->br();
+			$tag->imprime("<i>{$filter_result['descricao']}</i>");
+			$tag->br();
+			$tag->small();
+				$tag->a('href="'.ROOTPATHURL.$path.'view.php?id='.$filter_result['id'].'" target="blank" class="show-picture"');
+					$tag->imprime('Ver mais...');
+				$tag->a;
+			$tag->small;
+			$tag->br();
+			$tag->br();
+		$tag->td;
+	$tag->tr;
+}
+
+function bestiario($filter_result){
+	global $tag;
+	$path = ARTEFATOSPATH;
+	$tag->tr();
+		$tag->td();
+			$tag->span('class="search-title"');
+				$tag->a('href="'.ROOTPATHURL.$path.'view.php?id='.$filter_result['id'].'" target="blank"');
+					$tag->imprime($filter_result['nome']);
+				$tag->a;
+			$tag->span;
+			$tag->br();
+			$tag->imprime("Nível {$filter_result['lv']}, Sitema {$filter_result['sistema']}");
+			$tag->br();
+			$tag->imprime("Criado por {$filter_result['dono']}");
+			$tag->br();
+			$tag->imprime("<i>{$filter_result['descricao']}</i>");
+			$tag->br();
+			$tag->small();
+				$tag->a('href="'.ROOTPATHURL.$path.'view.php?id='.$filter_result['id'].'" target="blank" class="show-picture"');
+					$tag->imprime('Ver mais...');
+				$tag->a;
+			$tag->small;
+			$tag->br();
+			$tag->br();
+		$tag->td;
+	$tag->tr;
+}
