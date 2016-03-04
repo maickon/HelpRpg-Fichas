8ac87843dfbb42970905143bc16844e565f33230
maickon <maickonmaickon@hotmail.com>
2016-03-04 00:16:17 -0300

    mudanças no banco de dados local

--- db/db-config.php
+++ db/db-config.php
@@ -2,5 +2,5 @@
 //Usuario e senha do banco de dados
 define('DB_USER', "root");		
 define('DB_PASS', "");	  		
-define('DB_NAME', "helprpg"); 	
+define('DB_NAME', "help_db_producao"); 	
 define('DB_HOST', "localhost");
