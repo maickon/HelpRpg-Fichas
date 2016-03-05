f37c2323781d0cae2aa31be6a0e12195329c89ea
maickon <maickonmaickon@hotmail.com>
2016-03-05 12:51:56 -0300

    adicionando comentario na classe de sessao

--- class/sessao/session.class.php
+++ class/sessao/session.class.php
@@ -1,14 +1,16 @@
 <?php 
 class Session{
-
+	//O construtor ja cria uma sessao por padrao
 	function __construct(){
 		session_start();
 	}
 
+	//inicializa os parametros na sessao ativa para identifica la 
 	public function set_session($name, $value){
 		$_SESSION[$name] = $value;
 	}
 	 
+	//retorna o nome da sessao atual a fim de identificar se ela esta ativa
 	function get_session($name){
 		if(isset($_SESSION[$name])):
 			return $_SESSION[$name];
@@ -17,16 +19,19 @@ class Session{
 		endif;
 	}
 
+	//destroi a sessao ativa
 	function destroy_session($keys = array('login')){
 		foreach($keys as $key):
 			unset($_SESSION[$key]);
 		endforeach;
 	}
 
+	//destroi todas as sessoes ativas
 	function destroy_all_session(){
 		session_destroy();
 	}
 
+	//percorre a sessao atual a fim de identificar se determinao nome consta na sessao atual
 	function authenticate($name){
 		foreach($_SESSION as $key):
 			if($key == $_SESSION[$name]):
@@ -37,11 +42,16 @@ class Session{
 		endforeach;
 	}
 
+	//vei verificar se o nome passado por parametro consta na sessao atual 
+	//se for falso, redireciona para a pagina raiz
 	function restricted_access($url = ROOTPATHURL){
-		if(!$this->authenticate('login'))
+		if(!$this->authenticate('login')):
 			header('location:'.$url);
+		endif;
 	}
 
+	//verifica a integridade de determinado nome na sessao
+	//retorna false caso o nome nao exista 
 	public function exist($name) {
 		if(isset($_SESSION[$name]) && (!empty($_SESSION[$name]))):
 			return true;
@@ -50,6 +60,9 @@ class Session{
 		endif;
 	}
 	
+	//funcao usada para checar se os dados de user e senha conferem com a do DB
+	//caso nao exista user no db, retorna false
+	//do contrario, cria se uma sessao com o nome do seu login, senha e um ID de sessao e retorna true
 	public function login($email, $senha){
 		$senha_md5 = md5($senha);
 		$user = new Usuarios();
