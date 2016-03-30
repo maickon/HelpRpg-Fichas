fe60da804aba4779bcfb9cb3799ad5199ba07c0f
maickon <maickonmaickon@hotmail.com>
2016-03-30 00:13:48 -0300

    gerador de cidades, modulo piloto

---
+++ pages/cidades/index.php
@@ -0,0 +1,19 @@
+<script type="text/javascript" src="js/index.js"></script>
+<div id="main">
+
+<h1>Serendipity</h1>
+<h2>City Generator</h2>
+
+     <div id="blurb">
+          Roll up a teeming metropolis or three for your next fantasy setting.
+     </div>
+
+     <form name="GENFORM">
+               <textarea class="results" name="STORY" rows="4" cols="65"></textarea>
+               <br/><input type="button" class="go" name="BGENERATE" value="Generate City" onclick="GenPlot();">
+     </form>
+
+<p><strong>See also:</strong> <a href="stronghold.html">Stronghold</a>, <a href="tavern.html">Tavern Name</a>
+<br/><strong>Or:</strong> <a href="index.html">Main Page</a>
+
+</div>
---
+++ pages/cidades/js/index.js
@@ -0,0 +1,249 @@
+var aVocab = new Array();
+
+var intIncr
+var intCat
+
+intCat =0
+intIncr=0
+
+aVocab[intCat]= new Array();
+aVocab[intCat][0]="FIRST"
+aVocab[intCat][1] = new Array()
+aVocab[intCat][1][intIncr++]="Esta <SIZE>, <DENSITY> cidade construída predominantemente de <SUBSTANCE> e defendida por <DEFENSES>.  É bem conhecida pela sua <FEATURES>.  A maior parde de sua receita vem de <INDUSTRY>."
+aVocab[intCat][1][intIncr++]="Esta <SIZE> cidade é <DENSITY>.  Fica <LOCATION> e é contruido essencialmente da <SUBSTANCE>.  É defendida por <DEFENSES>, a sua característica mais notável é a <FEATURES>."
+aVocab[intCat][1][intIncr++]="Esta <SIZE>, <DENSITY> cidade <LOCATION> é mais conhecida como <REPUTE>.  A maioria de seus habitantes estão envolvidos em <INDUSTRY>, e é considerado notável pela sua <FEATURES>."
+
+intCat++
+intIncr=0
+aVocab[intCat]= new Array();
+aVocab[intCat][0]="SIZE"
+aVocab[intCat][1] = new Array()
+aVocab[intCat][1][intIncr++]="pequena cidade"	
+aVocab[intCat][1][intIncr++]="pequenina cidade"
+aVocab[intCat][1][intIncr++]="pequena cidade"	
+aVocab[intCat][1][intIncr++]="pequenina cidade"
+aVocab[intCat][1][intIncr++]="pequena cidade"	
+aVocab[intCat][1][intIncr++]="pequenina cidade"
+aVocab[intCat][1][intIncr++]="pequena cidade"	
+aVocab[intCat][1][intIncr++]="pequenina cidade"
+aVocab[intCat][1][intIncr++]="pequena cidade"	
+aVocab[intCat][1][intIncr++]="pequenina cidade"
+aVocab[intCat][1][intIncr++]="modesta"
+aVocab[intCat][1][intIncr++]="de tamanho médio"
+aVocab[intCat][1][intIncr++]="de tamanho moderado"
+aVocab[intCat][1][intIncr++]="razoavelmente grande"
+aVocab[intCat][1][intIncr++]="grande"
+aVocab[intCat][1][intIncr++]="vasto"
+
+intCat++
+intIncr=0
+aVocab[intCat]= new Array();
+aVocab[intCat][0]="DENSITY"
+aVocab[intCat][1] = new Array()
+aVocab[intCat][1][intIncr++] ="curiosamente vazia"
+aVocab[intCat][1][intIncr++] = "escassamente povoada"
+aVocab[intCat][1][intIncr++] = "não é muito populosa"
+aVocab[intCat][1][intIncr++] = "sonolento"
+aVocab[intCat][1][intIncr++] = "alastrando"
+aVocab[intCat][1][intIncr++] = "próspera"
+aVocab[intCat][1][intIncr++] = "movimentada"
+aVocab[intCat][1][intIncr++] = "bem povoada"
+aVocab[intCat][1][intIncr++] = "lotado"
+aVocab[intCat][1][intIncr++] = "superlotadas"
+
+intCat++
+intIncr=0
+aVocab[intCat]= new Array();
+aVocab[intCat][0]="SUBSTANCE"
+aVocab[intCat][1] = new Array()
+aVocab[intCat][1][intIncr++]="tijolo"
+aVocab[intCat][1][intIncr++]="tijolo vermelho"
+aVocab[intCat][1][intIncr++]="tijol ocinza"
+aVocab[intCat][1][intIncr++]="tijolos amarelos"
+aVocab[intCat][1][intIncr++]="pedra"
+aVocab[intCat][1][intIncr++]="granito"
+aVocab[intCat][1][intIncr++]="pedra"
+aVocab[intCat][1][intIncr++]="mármore"
+aVocab[intCat][1][intIncr++]="pedra vermelha"
+aVocab[intCat][1][intIncr++]="pedra negra"
+aVocab[intCat][1][intIncr++]="pedra clara"
+aVocab[intCat][1][intIncr++]="madeira"
+aVocab[intCat][1][intIncr++]="adobe"
+aVocab[intCat][1][intIncr++]="concreto"
+aVocab[intCat][1][intIncr++]="madeira"
+
+intCat++
+intIncr=0
+aVocab[intCat]= new Array();
+aVocab[intCat][0]="INDUSTRY"
+aVocab[intCat][1] = new Array()
+aVocab[intCat][1][intIncr++] = "têxteis"
+aVocab[intCat][1][intIncr++] = "artigos de couro"
+aVocab[intCat][1][intIncr++] = "pesca"
+aVocab[intCat][1][intIncr++] = "turismo"
+aVocab[intCat][1][intIncr++] = "metais"
+aVocab[intCat][1][intIncr++] = "comércio exterior"
+aVocab[intCat][1][intIncr++] = "mercado negro"
+aVocab[intCat][1][intIncr++] = "especiarias"
+aVocab[intCat][1][intIncr++] = "comércio de cavalos"
+aVocab[intCat][1][intIncr++] = "magia e feitiçaria"
+aVocab[intCat][1][intIncr++] = "mineração"
+aVocab[intCat][1][intIncr++] = "agricultura"
+aVocab[intCat][1][intIncr++] = "cerâmica"
+aVocab[intCat][1][intIncr++] = "marcenaria"
+aVocab[intCat][1][intIncr++] = "guerra"
+aVocab[intCat][1][intIncr++] = "contrabando"
+
+intCat++
+intIncr=0
+aVocab[intCat]= new Array();
+aVocab[intCat][0]="DEFENSES"
+aVocab[intCat][1] = new Array()
+aVocab[intCat][1][intIncr++] = "amazonas"
+aVocab[intCat][1][intIncr++] = "arqueiros elfos"
+aVocab[intCat][1][intIncr++]  = "magias arcanas"
+aVocab[intCat][1][intIncr++]  = "uma banda de elite de mercenários"
+aVocab[intCat][1][intIncr++]  = "um exército permanente"
+aVocab[intCat][1][intIncr++]  = "um exército formidável"
+aVocab[intCat][1][intIncr++]  = "a cidade guarda bem treinados"
+aVocab[intCat][1][intIncr++]  = "a cidade guarda um pouco indiferente"
+aVocab[intCat][1][intIncr++]  = "milícia local"
+aVocab[intCat][1][intIncr++]  = "paredes grossas"
+aVocab[intCat][1][intIncr++]  = "cravado paredes"
+aVocab[intCat][1][intIncr++]  = "elaborar paredes triplas"
+aVocab[intCat][1][intIncr++]  = "um fosso profundo e traiçoeiro"
+aVocab[intCat][1][intIncr++]  = "máquinas complexas"
+
+intCat++
+intIncr=0
+aVocab[intCat]= new Array();
+aVocab[intCat][0]="FEATURES"
+aVocab[intCat][1] = new Array()
+aVocab[intCat][1][intIncr++] = "quadrado grande e agitado mercado"
+aVocab[intCat][1][intIncr++] = "enorme templo de mármore"
+aVocab[intCat][1][intIncr++] = "exuberantes jardins públicos"
+aVocab[intCat][1][intIncr++] = "arquitetura única"
+aVocab[intCat][1][intIncr++] = "notavelmente mulheres bonitas"
+aVocab[intCat][1][intIncr++] = "monumento a um herói caído"
+aVocab[intCat][1][intIncr++] = "série de fontes elaboradas"
+aVocab[intCat][1][intIncr++] = "preponderância de estátuas equestres"
+aVocab[intCat][1][intIncr++] = "imponente torre do sino"
+aVocab[intCat][1][intIncr++] = "estilo arquitetônico eclético"
+aVocab[intCat][1][intIncr++] = "coloridos telhados"
+aVocab[intCat][1][intIncr++] = "museu de tesouros antigos"
+aVocab[intCat][1][intIncr++] = "ruas pavimentadas fancifully"
+aVocab[intCat][1][intIncr++] = "imenso centro de fort"
+aVocab[intCat][1][intIncr++] = "majestoso castelo"
+aVocab[intCat][1][intIncr++] = "impor aqueduto"
+aVocab[intCat][1][intIncr++] = "bela praça central"
+aVocab[intCat][1][intIncr++] = "quarter élfica colorido"
+aVocab[intCat][1][intIncr++] = "distrito da luz vermelha"
+aVocab[intCat][1][intIncr++] = "festival da colheita anual"
+aVocab[intCat][1][intIncr++] = "taxa de imposto ruinosa"
+aVocab[intCat][1][intIncr++] = "portas maciças entalhadas"
+aVocab[intCat][1][intIncr++] = "Tempo invariavelmente leve"
+aVocab[intCat][1][intIncr++] = "mausoléu pródigo"
+aVocab[intCat][1][intIncr++] = "murais detalhadas"
+aVocab[intCat][1][intIncr++] = "água excepcionalmente pura"
+aVocab[intCat][1][intIncr++] = "fontes termais"
+aVocab[intCat][1][intIncr++] = "labirinto subterrâneo"
+
+intCat++
+intIncr=0
+aVocab[intCat]= new Array();
+aVocab[intCat][0]="LOCATION"
+aVocab[intCat][1] = new Array()
+aVocab[intCat][1][intIncr++] = "lado de um rio"
+aVocab[intCat][1][intIncr++] = "junto ao mar"
+aVocab[intCat][1][intIncr++] = "montado em um rio"
+aVocab[intCat][1][intIncr++] = "perto da costa"
+aVocab[intCat][1][intIncr++] = "nas montanhas"
+aVocab[intCat][1][intIncr++] = "em uma importante rota comercial"
+aVocab[intCat][1][intIncr++] = "fundo de uma floresta"
+aVocab[intCat][1][intIncr++] = "à margem da civilização"
+aVocab[intCat][1][intIncr++] = "à beira de um deserto"
+aVocab[intCat][1][intIncr++] = "no coração do reino"
+aVocab[intCat][1][intIncr++] = "num vale profundo"
+
+intCat++
+intIncr=0
+aVocab[intCat]= new Array();
+aVocab[intCat][0]="REPUTE"
+aVocab[intCat][1] = new Array()
+aVocab[intCat][1][intIncr++] = "a pia da iniqüidade"
+aVocab[intCat][1][intIncr++] = "um centro de ciência e de educação"
+aVocab[intCat][1][intIncr++] = "um paraíso para as artes"
+aVocab[intCat][1][intIncr++] = "um buraco"
+aVocab[intCat][1][intIncr++] = "a meca cultural"
+aVocab[intCat][1][intIncr++] = "um remanso"
+aVocab[intCat][1][intIncr++] = "um playground para os ricos"
+aVocab[intCat][1][intIncr++] = "um bastião da neutralidade"
+aVocab[intCat][1][intIncr++] = "local de uma batalha histórica"
+aVocab[intCat][1][intIncr++] = "local de nascimento de um herói famoso"
+aVocab[intCat][1][intIncr++] = "o berço de um tirano notório"
+aVocab[intCat][1][intIncr++] = "a capital de um império caído"
+
+
+function GenNumber(nRange){
+	var iNumGen
+	iNumGen = Math.round((Math.random() * nRange));
+	return iNumGen;
+}
+
+function GetFrom(aArray){
+	var undefined
+	var sReturn
+	sReturn = aArray[GenNumber(aArray.length)];
+	if(sReturn == undefined){
+		sReturn = GetFrom(aArray)
+	}
+	return sReturn
+}
+
+function GetArray(sArrayName){
+	for(var intLooper=0;intLooper <aVocab.length;intLooper++){
+		if (aVocab[intLooper][0]==sArrayName){
+			return aVocab[intLooper][1];
+			break;
+		}
+	}
+}
+
+function ScanLine(sLine){
+	var iTagStart, iTagEnd
+	var sKey
+
+	if(sLine.indexOf("<") > -1){
+		iTagStart = sLine.indexOf("<");
+		iTagEnd = sLine.indexOf(">");
+
+		sKey = sLine.substr(iTagStart+1, iTagEnd-(iTagStart+1));
+
+		sKey = GetFrom(GetArray(sKey))
+		sLine = sLine.substr(0, iTagStart) + sKey + sLine.substr(iTagEnd+1, (sLine.length - iTagEnd))
+	}
+
+
+	if(sLine.indexOf("<") > - 1){
+		sLine = ScanLine(sLine)
+	}
+
+	return sLine;
+}
+
+//http://biroscanerd.blogspot.com.br/2011/12/dois-centavos-sobre-povoados-e-cidades.html
+var msg = "mensagem de <teste> para maickon";
+var i = msg.indexOf("<")
+var f = msg.indexOf(">")
+console.log(i+1);
+
+console.log(f-(i+1));
+
+sKey = sLine.substr(i+1, f-(i+1));
+console.log(sKey);
+
+function GenPlot(){
+	sLine = GetFrom(GetArray("FIRST"));
+	sLine = ScanLine(sLine)
+	document.GENFORM.STORY.value = sLine;
+}
