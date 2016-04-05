375a380f51ac685218ed4e8e9e9e4a96763cbfb3
maickon <maickonmaickon@hotmail.com>
2016-04-05 00:53:46 -0300

    fragmento de url para o novo modulo

--- rotas/rotas-painel.php
+++ rotas/rotas-painel.php
@@ -30,9 +30,11 @@ define('PERICIASIMGPATH', "img/pericias/");
 define('FICHASUPLOADPATH', "pages/painel/upload-fichas/");
 define('FICHASUPLOADFILEPATH', "file/upload-fichas/");
 
+define('PERSONAGEMPATHLINK', "pages/painel/upload-fichas/");
 define('PERSONAGEMPATH', "pages/painel/personagens/");
 define('PERSONAGEMIMGPATH', "img/personagens/");
 
+define('MONSTROPATHLINK', "pages/painel/upload-fichas/");
 define('MONSTROPATH', "pages/painel/monstros/");
 define('MONSTROIMGPATH', "img/monstros/");
 
