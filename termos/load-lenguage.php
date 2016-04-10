8af6cc6dcebb28605d772e61e78f4a255aa4abe7
maickon <maickonmaickon@hotmail.com>
2016-04-09 23:04:39 -0300

    melhorias no arquivos de carregamento de idioma

--- termos/load-lenguage.php
+++ termos/load-lenguage.php
@@ -5,6 +5,7 @@ $s = new Session();
 if(isset($_GET['l'])):
 	if(isset($_SESSION['linguagem'])):
 		$s->set_session('linguagem', $_GET['l']);
+		
 	else:
 		$s->set_session('linguagem', $_GET['l']);
 	endif;
@@ -40,4 +41,19 @@ switch($s->get_session('linguagem')):
 			require_once 'en/pericias.php';
 			require_once 'en/login.php';
 		break;
+	
+	default:
+		$s->set_session('linguagem', 'pt-br');
+		require_once 'pt-br/geral.php';
+		require_once 'pt-br/3det.php';
+		require_once 'pt-br/d20.php';
+		require_once 'pt-br/deamon.php';
+		require_once 'pt-br/d20-ded-4.0.php';
+		require_once 'pt-br/d20-ded-5.0.php';
+		require_once 'pt-br/fate.php';
+		require_once 'pt-br/itens.php';
+		require_once 'pt-br/magias.php';
+		require_once 'pt-br/pericias.php';
+		require_once 'pt-br/login.php';
+	break;
 endswitch;
