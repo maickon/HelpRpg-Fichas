f3c2c4cdfb055a8e57c5b51570d3d5b86f9b4cd5
maickon <maickonmaickon@hotmail.com>
2016-04-09 23:03:19 -0300

    definindo variaveis na URL para gerenciar idioma do site

--- rotas/rotas-fragmentos.php
+++ rotas/rotas-fragmentos.php
@@ -3,5 +3,12 @@
 define('ID', "id");
 define('VIEW_BY_ID', "view.php?".ID."=");
 define("CONTADOR_MAIS_CEM", "100+");
-define('PTBR_PATH', PHP_SELF.'?l=pt-br');
-define('EN_PATH', PHP_SELF.'?l=en');
+
+$url = explode('/', $_SERVER['REQUEST_URI']);
+if(empty($url[count($url)-1])):
+	define('PTBR_PATH', '?l=pt-br');
+	define('EN_PATH', '?l=en');	
+else:
+	define('PTBR_PATH', $_SERVER['REQUEST_URI'].'&l=pt-br');
+	define('EN_PATH', $_SERVER['REQUEST_URI'].'&l=en');
+endif;
