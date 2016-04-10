dc4262ebc3b7dec51d89fed7ea6910f30a300905
maickon <maickonmaickon@hotmail.com>
2016-04-09 23:01:26 -0300

    add novos campos a pagina de usuario

--- pages/usuario/edit.php
+++ pages/usuario/edit.php
@@ -21,6 +21,7 @@ if(isset($_REQUEST['action'])):
 	$form->row_();
 endif;
 
+$tag->link('href="'.CSSPATH.'index.css" rel="stylesheet"');
 $tag->br();
 
 $form->_row();
@@ -37,24 +38,44 @@ $form->row_();
 
 
 $form->_row();
-	$form->_container();		
-		$form->_form(['method'=>'post', 'name'=>'new-user', 'class'=>'form-group', 'data-toggle'=>'validator']);
-		
-			helper_form_input("Nome", ['name' => 'nome', 'type' => 'text', 'class'=>'form-control', 'required'=>'true', 'value'=> $user[0]['nome']]);
-			
-			helper_form_input("Login", ['name' => 'login', 'type' => 'text', 'class'=>'form-control', 'required'=>'true', 'value'=> $user[0]['login']]);
+	$form->_container();	
+		$form->_form(['method'=>'post', 'name'=>'new-user', 'id'=>'form_user_edit', 'class'=>'form-group', 'data-toggle'=>'validator'], 12);
+			$form->_col(4);	
+				helper_form_input(NOME, ['name' => 'nome', 'id' => 'nome', 'type' => 'text', 'class'=>'form-control', 'value'=> $user[0]['nome']], 12);
 				
-			helper_form_input("Email", ['name' => 'email', 'type' => 'email', 'id'=>'inputEmail', 'class'=>'form-control', 'data-error'=>'Este email não é válido!', 'required'=>'true', 'value'=> $user[0]['email']]);
-			
-			helper_form_input("Senha", ['name' => 'senha', 'id'=>'senha', 'type' => 'password', 'class'=>'form-control', 'required'=>'true']);
-			
-			helper_form_input("Confirmar Senha", ['name' => 'confirma', 'id'=>'senha_confirmar', 'type' => 'password', 'class'=>'form-control', 'required'=>'true']);
-			
-			helper_form_button_update_and_back(ROOTPATHURL);
-		
+				helper_form_input(NICK, ['name' => 'login', 'id' => 'login', 'type' => 'text', 'class'=>'form-control', 'value'=> $user[0]['login']], 12);
+					
+				helper_form_input(EMAIL, ['name' => 'email', 'id' => 'email', 'type' => 'email', 'class'=>'form-control', 'value'=> $user[0]['email']], 12);
+				
+				helper_form_input(SENHA, ['name' => 'senha', 'id'=>'senha', 'type' => 'password', 'class'=>'form-control'], 12);
+				
+				helper_form_input(CONFIRMA, ['name' => 'confirma', 'id'=>'confirma', 'type' => 'password', 'class'=>'form-control'], 12);
+			$form->col_();
+
+			$form->_col(4);
+				helper_form_input('Facebook Link', ['name' => 'facebook_link', 'id' => 'facebook_link', 'type' => 'text', 'class'=>'form-control', 'value'=> $user[0]['facebook_link']], 12);
+
+				helper_form_input('Twitter Link', ['name' => 'twitter_link', 'id' => 'twitte_link', 'type' => 'text', 'class'=>'form-control', 'value'=> $user[0]['twitter_link']], 12);
+
+				helper_form_input('G+ Link', ['name' => 'gplus_link', 'id' => 'gplus_link', 'type' => 'text', 'class'=>'form-control', 'value'=> $user[0]['gplus_link']], 12);
+
+				helper_form_input('Fanpage', ['name' => 'pagina_social', 'id' => 'pagina_social', 'type' => 'text', 'class'=>'form-control', 'value'=> $user[0]['pagina_social']], 12);
+
+				helper_form_input('Site Próprio', ['name' => 'site_pessoal', 'id' => 'site_pessoal', 'type' => 'text', 'class'=>'form-control', 'value'=> $user[0]['site_pessoal']], 12);
+			$form->col_();
+
+			$form->_col(4);
+				helper_form_text_area("Descrição breve sobre você", ['name' => 'descricao', 'class'=>'form-control', 'ckeditor'=>'off', 'rows'=>'15'], strip_tags($user[0]['descricao']));
+			$form->col_();
+
+			$form->_col(12);	
+				helper_form_button_update_and_back(ROOTPATHURL, 12);
+			$form->col_();
+
 		$form->form_();
-		
 	$form->_container();
 $form->row_();
 $tag->div;
+
+$tag->script('src="js/index.js"'); $tag->script;
 require_once '../../footer.php';
