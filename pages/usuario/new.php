28f140a407c330e2f670db84a19c5c34a5f42942
maickon <maickonmaickon@hotmail.com>
2016-04-09 23:02:23 -0300

    melhorias na pagina de cadastro de usuarios

--- pages/usuario/new.php
+++ pages/usuario/new.php
@@ -9,36 +9,54 @@ $form = new Form();
 
 if(isset($_REQUEST['action'])):
 	$create_user = new Usuarios();
-	$form->_row();
-		$form->_container();
+	$tag->br();
+	$form->_container();
+		$form->_col(12);
 			$create_user->create($_REQUEST);
-		$form->container_();
-	$form->row_();
+		$form->col_();
+	$form->container_();
 endif;
 
-$tag->script('src="js/index.js"'); $tag->script;
+$tag->link('href="'.CSSPATH.'index.css" rel="stylesheet"');
 
 $form->_row();
 	$form->_container();
-		helper_adsense_01();
+		$form->_col(4);
+		
+		$form->col_();	
+
+		$form->_col(4);
+			$form->_col(12);
+				$tag->h1('class="helper_txt"');
+					$tag->imprime(CADASTRO_USER_MSG);
+				$tag->h1;
+			$form->col_();
+			
+			$form->_col(12);
+				helper_adsense_responsivo_02();
+			$form->col_();
+			
+			$form->_form(['method'=>'post', 'id'=>'form_user', 'name'=>'new-user', 'class'=>'form-group', 'data-toggle'=>'validator']);
 				
-		$form->_form(['method'=>'post', 'id'=>'form', 'name'=>'new-user', 'class'=>'form-group', 'data-toggle'=>'validator']);
-		
-			helper_form_input("Nome", ['name' => 'nome', 'type' => 'text', 'class'=>'form-control', 'required'=>'true']);
-			
-			helper_form_input("Nick", ['name' => 'login', 'type' => 'text', 'class'=>'form-control', 'required'=>'true']);
+				helper_form_input(NOME, ['id' => 'nome','name' => 'nome', 'type' => 'text', 'class'=>'form-control'], 12);
 				
-			helper_form_input("Email", ['name' => 'email', 'type' => 'email', 'id'=>'inputEmail', 'class'=>'form-control', 'data-error'=>'Este email não é válido!', 'required'=>'true']);
+				helper_form_input(NICK, ['id' => 'login','name' => 'login', 'type' => 'text', 'class'=>'form-control'], 12);
+					
+				helper_form_input(EMAIL, ['id' => 'email','name' => 'email', 'type' => 'email', 'id'=>'inputEmail', 'class'=>'form-control', 'data-error'=>'Este email não é válido!'], 12);
+				
+				helper_form_input(SENHA, ['id' => 'senha','name' => 'senha', 'id'=>'senha', 'type' => 'password', 'class'=>'form-control'], 12);
+				
+				helper_form_input(CONFIRMA, ['id' => 'confirma','name' => 'confirma', 'id'=>'senha_confirmar', 'type' => 'password', 'class'=>'form-control'], 12);
+				
+				helper_form_button_submit_and_back(ROOTPATHURL, 12);
 			
-			helper_form_input("Senha", ['name' => 'senha', 'id'=>'senha', 'type' => 'password', 'class'=>'form-control', 'required'=>'true']);
-			
-			helper_form_input("Confirmar Senha", ['name' => 'confirma', 'id'=>'senha_confirmar', 'type' => 'password', 'class'=>'form-control', 'required'=>'true']);
-			
-			helper_form_button_submit_and_back(ROOTPATHURL);
-		
-		$form->form_();
-		
+			$form->form_();
+
+		$form->col_();	
+
 	$form->_container();
 $form->row_();
 $tag->div;
+
+$tag->script('src="js/index.js"'); $tag->script;
 require_once '../../footer.php';
