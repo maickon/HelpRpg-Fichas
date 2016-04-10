6c565b1368db8076c1a542d612c3d6a787ab759a
maickon <maickonmaickon@hotmail.com>
2016-04-09 23:02:06 -0300

    add novo arquivo javascript para validar formulario de usuarios

--- pages/usuario/js/index.js
+++ pages/usuario/js/index.js
@@ -1,37 +1,86 @@
- $('#form').validate({
-        rules: {
-            nome: {
-                required: true,
-                minlength: 5
-            },
-            email: {
-                required: true,
-                email: true
-            },
-            senha: {
-                required: true
-            },
-            confirma: {
-                required: true,
-                equalTo: "#senha"
-            }
-        },
-        messages: {
-            nome: {
-                required: "O campo nome é obrigatório.",
-                minlength: "O campo nome deve conter no mínimo 5 caracteres."
-            },
-            email: {
-                required: "O campo email é obrigatório.",
-                email: "O campo email deve conter um email válido."
-            },
-            senha: {
-                required: "O campo senha é obrigatório."
-            },
-            confirma: {
-                required: "O campo confirmação de senha é obrigatório.",
-                equalTo: "O campo confirmação de senha deve ser identico ao campo senha."
-            }
-        }
-
-    });
+$(document).ready( function() {
+  $("#form_user, #form_user_edit").validate({
+    // Define as regras
+    rules:{
+      nome:{
+        // campoNome será obrigatório (required) e terá tamanho mínimo (minLength)
+        required: true, minlength: 3
+      },
+      login:{
+        // campologin será obrigatório (required) e terá tamanho mínimo (minLength)
+        required: true, minlength: 3
+      },
+      email:{
+        // campoEmail será obrigatório (required) e precisará ser um e-mail válido (email)
+        required: true, email: true
+      },
+      senha:{
+        // campoMensagem será obrigatório (required) e terá tamanho mínimo (minLength)
+        required: true, minlength: 8
+      },
+      confirma:{
+        // campoMensagem será obrigatório (required) e terá tamanho mínimo (minLength)
+        required: true, minlength: 8, equalTo: "#senha"
+      },
+      facebook_link:{
+        // campo URL terá que ser uma url valida
+        url: true
+      },
+      twitter_link:{
+        // campo URL terá que ser uma url valida
+        url: true
+      },
+      gplus_link:{
+        // campo URL terá que ser uma url valida
+        url: true
+      },
+      pagina_social:{
+        // campo URL terá que ser uma url valida
+        url: true
+      },
+      site_pessoal:{
+        // campo URL terá que ser uma url valida
+        url: true
+      }
+    },
+    // Define as mensagens de erro para cada regra
+    messages:{
+      nome:{
+        required: "Digite o seu nome",
+        minLength: "O seu nome deve conter, no mínimo, 3 caracteres"
+      },
+      login:{
+        required: "Digite o seu nick (Apelido)",
+        minLength: "O seu nick deve conter, no mínimo, 3 caracteres"
+      },
+      email:{
+        required: "Digite o seu e-mail para contato",
+        email: "Digite um e-mail válido"
+      },
+      senha:{
+        required: "Digite a sua senha",
+        minLength: "A sua senha deve conter, no mínimo, 8 caracteres"
+      },
+      confirma:{
+        required: "Confirme a sua senha",
+        minLength: "A sua senha deve conter, no mínimo, 8 caracteres",
+        equalTo: "As senhas devem ser iguais"
+      },
+      facebook_link:{
+        url: "URL inválida"
+      },
+      twitter_link:{
+        url: "URL inválida"
+      },
+      gplus_link:{
+        url: "URL inválida"
+      },
+      pagina_social:{
+        url: "URL inválida"
+      },
+      site_pessoal:{
+        url: "URL inválida"
+      }
+    }
+  });
+});
